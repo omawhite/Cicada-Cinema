bc1c88bb31ee1eeb14c81aeb9ce957ad5f691e45
Omar Louis White <louiswhite3019@icloud.com>
2026-04-12 22:20:04 -0400

    chore: storybook setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import eslintPluginAstro from "eslint-plugin-astro";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  ...eslintPluginAstro.configs["flat/recommended"],
-  {
-    ignores: ["dist/", ".astro/"],
-  },
-);
+export default tseslint.config(...eslintPluginAstro.configs["flat/recommended"], {
+  ignores: ["dist/", ".astro/"],
+}, storybook.configs["flat/recommended"]);
