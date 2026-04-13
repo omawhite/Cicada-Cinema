ab792507f36abe97b6b333c05ca62808bb7f36a9
Omar Louis White <louiswhite3019@icloud.com>
2026-04-12 21:28:38 -0400

    chore: ran pnpm astro add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
+  integrations: [react()],
 });
