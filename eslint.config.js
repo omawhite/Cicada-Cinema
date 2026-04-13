ea1312127b0b1cfce472a23030305fd1e25ac7c2
Omar Louis White <louiswhite3019@icloud.com>
2026-04-12 22:44:04 -0400

    fix: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,15 @@ import storybook from "eslint-plugin-storybook";
 
 import eslintPluginAstro from "eslint-plugin-astro";
 import tseslint from "typescript-eslint";
+import { defineConfig } from 'eslint/config';
 
-export default tseslint.config(...eslintPluginAstro.configs["flat/recommended"], {
-  ignores: ["dist/", ".astro/"],
-}, storybook.configs["flat/recommended"]);
+
+export default defineConfig(
+  eslintPluginAstro.configs["flat/recommended"],
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  storybook.configs["flat/recommended"],
+  {
+    ignores: ["dist/", ".astro/", ".claude/"],
+  },
+)
