5b466287e0cda1ca8d0edac26c4f8a97e7761f2d
Omar Louis White <louiswhite3019@icloud.com>
2026-05-17 19:46:29 -0400

    chore: updates story section order

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,6 +4,11 @@ import { withThemeByClassName } from "@storybook/addon-themes";
 
 const preview: Preview = {
   parameters: {
+    options: {
+      storySort: {
+        order: ["Design Tokens", "UI", "Layout", "Components"],
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
