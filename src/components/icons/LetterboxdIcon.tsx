3ff946b9d3b996e0defbc2fc8bc10b58c3d99ab4
Omar Louis White <louiswhite3019@icloud.com>
2026-05-17 20:29:08 -0400

    fix: letterbox icon to better match design

--- src/components/icons/LetterboxdIcon.tsx
+++ src/components/icons/LetterboxdIcon.tsx
@@ -6,14 +6,14 @@ export function LetterboxdIcon({ className }: LetterboxdIconProps) {
   return (
     <svg
       className={className}
-      viewBox="0 0 30 30"
+      viewBox="0 0 46 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
     >
-      <circle cx="7" cy="15" r="8" fill="#40BCF4" />
-      <circle cx="15" cy="15" r="8" fill="#00C030" />
-      <circle cx="23" cy="15" r="8" fill="#FF8000" />
+      <circle cx="10" cy="10" r="10" fill="#40BCF4" />
+      <circle cx="23" cy="10" r="10" fill="#00C030" />
+      <circle cx="36" cy="10" r="10" fill="#FF8000" />
     </svg>
   );
 }
